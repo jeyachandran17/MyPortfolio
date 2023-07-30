e511c83d92a39a0eada10dda265c071e4b154c45
jeyachandran17 <jeyachandran1733@gmail.com>
2023-07-30 22:50:27 +0530

    created in NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,46 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <Application />
+    </div>
   )
 }
+
+
+function Application() {
+  return (
+    <div className="application">
+      <div className="navigation-container">
+        <Navigation />
+      </div>
+    </div>
+  );
+}
+
+
+function Navigation() {
+  return (
+    <div className="navigation">
+      <nav>
+      <AppBar position="static">
+        <Toolbar>
+          {/* create the links for webpage */}
+        </Toolbar>
+      </AppBar>
+      </nav>
+    </div>
+  );
+}
+
 
 export default App
