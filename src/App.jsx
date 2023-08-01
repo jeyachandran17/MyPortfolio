51529ade5e7b15f027a003fc7d8b53cdd5713346
jeyachandran17 <jeyachandran1733@gmail.com>
2023-08-01 11:05:00 +0530

    add title & home page

--- src/App.jsx
+++ src/App.jsx
@@ -33,12 +33,31 @@ function Navigation() {
   return (
     <div className="navigation">
       <nav>
-      <AppBar position="static">
+      <AppBar style={{background:"transparent"}} position="static">
         <Toolbar>
-          {/* create the links for webpage */}
+            {/* create the links for webpage */}
+            <p><span className='fn'>Jeya</span><span className='ln'>chandran C</span></p>
+            <Link className='link' to={"/"} style={{marginLeft:"auto"}}>Home</Link>
         </Toolbar>
       </AppBar>
       </nav>
+      <div className="profile">
+        <Profile />
+      </div>
+    </div>
+  );
+}
+
+
+function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-content">
+        <p>Welcome to my page...</p>
+        <p>I'm JEYACHANDRAN</p>
+        <p>I'm Mean stack devoloper and Motivational speeker,</p>
+        <p>Everytime i work at try to my best and solve the problems.</p>
+      </div>
     </div>
   );
 }
