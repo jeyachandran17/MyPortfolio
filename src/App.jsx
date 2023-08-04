1d1e1828835f1380d97c1d7ce235a720c1737f31
jeyachandran17 <jeyachandran1733@gmail.com>
2023-08-04 22:30:38 +0530

    added contact field

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ import GitIcon from './images/git.png'
 import HTML5Icon from './images/html.png'
 import CSS3Icon from './images/css3.png'
 import JSIcon from './images/javascript.png'
-
-
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 
 function App() {
@@ -46,13 +46,15 @@ function Navigation() {
         <Toolbar>
             {/* create the links for webpage */}
             <p><span className='fn'>Jeya</span><span className='ln'>chandran C</span></p>
-            <Link className='link' to={"/"} style={{marginLeft:"auto"}}>Home</Link>
+            <a className='link' href="/" style={{marginLeft:"auto"}}>Home</a>
+            <a className='link' href="#contect" >Contect</a>
         </Toolbar>
       </AppBar>
       </nav>
       <div className="profile">
         <Profile />
         <SkillAndTools />
+        <Contect />
       </div>
     </div>
   );
@@ -112,6 +114,35 @@ function SkillAndTools() {
           <img className='iconImage' src={JSIcon} alt="JavaScript" />
           <h3>JavaScript</h3>
         </div>
+      </div>
+    </div>
+  );
+}
+
+function Contect() {
+  return (
+    <div id='contect' className="contect-container">
+      <div className="contect-title-container">
+        <h2>Contect Us</h2>
+      </div>
+      <div className="socialmedia-container">
+        <div className="media-follow-container">
+          <div className="follow-title-container">
+            <h3>Follow</h3>
+          </div>
+          <div className="follow-link-container">
+            <a href="https://github.com/jeyachandran17"><GitHubIcon fontSize='large' /></a>
+            <a href="https://www.linkedin.com/in/jeyachandran-c-604186265/"><LinkedInIcon fontSize='large' /></a>
+          </div>
+        </div>
+        <div className="mail-container">
+          <h3>Chat to us</h3>
+          <p className='mail-address'><MailOutlineIcon fontSize='large' />jeyachandran1733@gmail.com</p>
+        </div>
+      </div>
+      <div className="copyright-container">
+        <p><span className='fn'>Jeya</span><span className='ln'>chandran C</span></p>
+        <h3>&copy; 2023 Jeyachandran C. All rights reserved.</h3>
       </div>
     </div>
   );
